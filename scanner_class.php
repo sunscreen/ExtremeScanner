8137887fa9d7769851da0b9467af7217a598c47d
sunscreen <ncoredump@gmail.com>
2023-04-15 14:31:54 +0100

    Update scanner_class.php

--- scanner_class.php
+++ scanner_class.php
@@ -1,30 +1,4 @@
 <?php
-include('scanner_class.php');
-include('scanner_functions.php');
-
-//error_reporting(~E_ALL ^ ~E_WARNING);
-
-$community = "public";
-$seedips = [];
-$networkips = [];
-$extreme_scanner = new ExtremeScanner();
-$extreme_scanner->load_oids();
-
-$readlines = file("seedips.txt");
-
-foreach ($readlines as $txtline)
-{
-    array_push($seedips, trim($txtline));
-}
-
-$networkips=ipscan_harvest($extreme_scanner,$seedips);
-collect_devices($extreme_scanner,$networkips);
-exit(0);
-
-
-?>
-root@bhr:~# cat scanner_class.php
-<?php
 class ExtremeScanner
 {
     public $scanaddress;
